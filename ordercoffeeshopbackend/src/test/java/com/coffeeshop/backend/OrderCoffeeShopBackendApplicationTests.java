f11e60a21de0b8128e6b07499fdef14bfe1cc43d
daothuan <td2005dataeg@gmail.com>
2025-10-07 09:17:59 +0700

    backend init

---
+++ ordercoffeeshopbackend/src/test/java/com/coffeeshop/backend/OrderCoffeeShopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coffeeshop.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderCoffeeShopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
